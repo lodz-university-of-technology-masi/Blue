8738e8ab77d6d1d84a41506c1244494f752681d5
Maciej Wyrzuc <wyrzuc.maciej@gmail.com>
2019-03-27 13:02:08 +0100

    Initial ide project architecture

---
+++ backend/src/main/java/com/masiblue/backend/RekrutacjaServerApplication.java
@@ -0,0 +1,13 @@
+package com.masiblue.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RekrutacjaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RekrutacjaServerApplication.class, args);
+	}
+
+}
