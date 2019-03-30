67ead4b7fe5980898a38336f41594b7b877afdaf
Maciej Wyrzuc <wyrzuc.maciej@gmail.com>
2019-03-30 15:12:15 +0100

    Added docker support

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,19 @@
 module.exports = { 
-    // Change build paths to make them Maven compatible
-    // see https://cli.vuejs.org/config/
-    outputDir: 'target/dist',
-    assetsDir: 'static'
+  // Change build paths to make them Maven compatible
+  // see https://cli.vuejs.org/config/
+  outputDir: 'target/dist',
+  assetsDir: 'static',
+
+  // proxy all webpack dev-server requests starting with /api
+  // to our Spring Boot backend (localhost:8088) using http-proxy-middleware
+  // see https://cli.vuejs.org/config/#devserver-proxy
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8088',
+        ws: true,
+        changeOrigin: true
+      }
+    }
   }
+}
