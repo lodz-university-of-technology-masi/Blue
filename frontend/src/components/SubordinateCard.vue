b9fee7e1de9ec7d66e5ee43f1535403ca9fafce8
Joanna Górczak <203203@edu.p.lodz.pl>
2019-04-08 01:28:55 +0200

    Basic UI for postions management

---
+++ frontend/src/components/SubordinateCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="card bg-light mb-3 list-item">
+    <div class="card-body">
+      <b-row no-gutters>
+        <b-col>
+          <h5 class="card-title subordinate-text">{{subordinate.firstName}} {{subordinate.lastName}}</h5>
+        </b-col>
+        <b-col>
+          <p class="card-text subordinate-text">{{subordinate.role}}</p>
+        </b-col>
+        <b-col>
+          <b-button href="#" class="button-left-margin" variant="primary">Edit</b-button>
+          <b-button href="#" class="button-left-margin" variant="danger">Delete</b-button>
+        </b-col>
+      </b-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SubordinateCard",
+  props: {
+    subordinate: Object
+  }
+};
+</script>
+
+<style scoped>
+
+.subordinate-text {
+  font-weight: 550;
+}
+
+.list-item {
+  margin-left: 15%;
+  margin-right: 15%;
+}
+
+.button-left-margin {
+  margin-left: 10px;
+}
+</style>
