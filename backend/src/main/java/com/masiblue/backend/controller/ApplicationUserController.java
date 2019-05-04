aec02b3e89e53f1015b937969160aad30f5d527b
Maciej Wyrzuc <wyrzuc.maciej@gmail.com>
2019-05-04 19:40:21 +0200

    Changed admin to moderator and moderator to redactor

--- backend/src/main/java/com/masiblue/backend/controller/ApplicationUserController.java
+++ backend/src/main/java/com/masiblue/backend/controller/ApplicationUserController.java
@@ -1,6 +1,14 @@
 package com.masiblue.backend.controller;
 
+import com.masiblue.backend.exception.ApplicationUserNotFoundException;
+import com.masiblue.backend.exception.RedactorNotFoundException;
+import com.masiblue.backend.exception.UserAccountAlreadyExistsException;
+import com.masiblue.backend.model.ApplicationUser;
+import com.masiblue.backend.model.UserAccountDTO;
 import com.masiblue.backend.service.ApplicationUserService;
+import com.masiblue.backend.service.UserAccountService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,14 +19,75 @@ import java.util.List;
 public class ApplicationUserController {
 
     private final ApplicationUserService applicationUserService;
+    private final UserAccountService userAccountService;
 
-    public ApplicationUserController(ApplicationUserService applicationUserService) {
+    public ApplicationUserController(ApplicationUserService applicationUserService, UserAccountService userAccountService) {
         this.applicationUserService = applicationUserService;
+        this.userAccountService = userAccountService;
     }
 
-    @PreAuthorize("hasAnyRole('ADMIN', 'MODERATOR')")
+    @PreAuthorize("hasRole('MODERATOR')")
     @GetMapping
     public List listUsers() {
         return applicationUserService.findAll();
     }
+
+    @PreAuthorize("hasRole('MODERATOR')")
+    @PostMapping("/redactors")
+    public ResponseEntity<String> addRedactor(@RequestBody UserAccountDTO userAccountDTO) {
+        try {
+            userAccountService.addNewRedactor(userAccountDTO);
+            return new ResponseEntity<>("Successfully created new moderator", HttpStatus.OK);
+        } catch (UserAccountAlreadyExistsException ex) {
+            return new ResponseEntity<>("Account with this username already exists", HttpStatus.BAD_REQUEST);
+        }
+
+
+    }
+
+    @PreAuthorize("hasRole('MODERATOR')")
+    @GetMapping("/redactors")
+    public List listRedactors() {
+        return applicationUserService.findAllRedactors();
+    }
+
+    @PreAuthorize("hasRole('MODERATOR')")
+    @GetMapping("/redactors/{id}")
+    public ResponseEntity listSingleRedactor(@PathVariable("id") long id) {
+        try {
+            ApplicationUser moderator = applicationUserService.findRedactorById(id);
+            return new ResponseEntity<>(moderator, HttpStatus.OK);
+        } catch (ApplicationUserNotFoundException ex) {
+            return new ResponseEntity<>("There is no user with this id", HttpStatus.BAD_REQUEST);
+        } catch (RedactorNotFoundException e) {
+            return new ResponseEntity<>("User with this id is not a moderator", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PreAuthorize("hasRole('MODERATOR')")
+    @PutMapping("/redactors/{id}")
+    public ResponseEntity<String> updateRedactor(@PathVariable("id") long id, @RequestBody ApplicationUser newUserData) {
+        try {
+            applicationUserService.updateRedactor(id, newUserData);
+            return new ResponseEntity<>("Successfully updated moderator data", HttpStatus.OK);
+        } catch (RedactorNotFoundException e) {
+            return new ResponseEntity<>("User with this id is not a moderator", HttpStatus.BAD_REQUEST);
+        } catch (ApplicationUserNotFoundException e) {
+            return new ResponseEntity<>("There is no user with this id", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PreAuthorize("hasRole('MODERATOR')")
+    @DeleteMapping("/redactors/{id}")
+    public ResponseEntity<String> deleteRedactor(@PathVariable("id") long id) {
+        try {
+            applicationUserService.deleteRedactor(id);
+            return new ResponseEntity<>("Successfully deleted moderator data", HttpStatus.OK);
+        } catch (RedactorNotFoundException e) {
+            return new ResponseEntity<>("User with this id is not a moderator", HttpStatus.BAD_REQUEST);
+        } catch (ApplicationUserNotFoundException e) {
+            return new ResponseEntity<>("There is no user with this id", HttpStatus.BAD_REQUEST);
+        }
+    }
+
 }
--- backend/src/main/java/com/masiblue/backend/repository/RoleRepository.java
+++ backend/src/main/java/com/masiblue/backend/repository/RoleRepository.java
@@ -1,6 +1,5 @@
 package com.masiblue.backend.repository;
 
-
 import com.masiblue.backend.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
