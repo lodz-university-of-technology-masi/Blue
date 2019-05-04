fdfafab114ddfef1379a53974eae1cf0991aa783
Joanna Górczak <203203@edu.p.lodz.pl>
2019-05-05 00:42:39 +0200

    Added possibility to add new redactor and delete existing - connected to backend

--- frontend/src/views/ManageRedactors.vue
+++ frontend/src/views/ManageRedactors.vue
@@ -1,10 +1,77 @@
 <template>
-  <div class="manage_positions">
-    <ul id="subordinatesList">
-      <li v-for="subordinate in subordinates" :key="subordinate.id">
-        <SubordinateCard @refreshSubordinates="getSubordinates" :subordinate="subordinate"></SubordinateCard>
-      </li>
-    </ul>
+  <div>
+    <div class="manage_positions">
+      <button
+        type="button"
+        @click="initNewRedactorModalValues"
+        class="margin-bottom btn btn-success btn-lg"
+      >Add new redactor</button>
+      <ul id="subordinatesList">
+        <li v-for="subordinate in subordinates" :key="subordinate.id">
+          <SubordinateCard @refreshSubordinates="getSubordinates" :subordinate="subordinate"></SubordinateCard>
+        </li>
+      </ul>
+    </div>
+
+    <div>
+      <b-modal class="add-redactor-modal" v-model="addRedactorModalShow" title="Add new redactor">
+        <div>
+          <b-row class="bottom-margin">
+            <b-col sm="3">
+              <label>First name</label>
+            </b-col>
+            <b-col sm="9">
+              <b-form-input
+                v-model="inputFirstName"
+                class="bottom-margin"
+                :title="'First name'"
+              ></b-form-input>
+            </b-col>
+          </b-row>
+
+          <b-row class="bottom-margin">
+            <b-col sm="3">
+              <label>Last name</label>
+            </b-col>
+            <b-col sm="9">
+              <b-form-input
+                v-model="inputLastName"
+                class="bottom-margin"
+                :title="'Last name'"
+              ></b-form-input>
+            </b-col>
+          </b-row>
+
+          <b-row class="bottom-margin">
+            <b-col sm="3">
+              <label>Username</label>
+            </b-col>
+            <b-col sm="9">
+              <b-form-input
+                v-model="inputUsername"
+                class="bottom-margin"
+                :title="'Username'"
+              ></b-form-input>
+            </b-col>
+          </b-row>
+
+          <b-row class="bottom-margin">
+            <b-col sm="3">
+              <label>Password</label>
+            </b-col>
+            <b-col sm="9">
+              <b-form-input
+                v-model="inputPassword"
+                class="bottom-margin"
+                :title="'Password'"
+                type = 'password'
+              ></b-form-input>
+            </b-col>
+          </b-row>
+        </div>
+        <div slot="modal-ok" @click="addRedactor">Save</div>
+      </b-modal>
+    </div>
   </div>
 </template>
 
@@ -19,38 +86,93 @@ export default {
     return {
       subordinates: [],
       loading: true,
-      availableRoles: [],
+      addRedactorModalShow: false,
+      inputFirstName: "",
+      inputLastName: "",
+      inputUsername: "",
+      inputPassword: "",
+      newRedactor: {
+        username: "",
+        password: "",
+        firstName: "",
+        lastName: ""
+      }
     };
   },
   methods: {
-    getSubordinates: function () {
+    initNewRedactorModalValues: function() {
+      this.inputFirstName = "";
+      this.inputLastName = "";
+      this.inputUsername = "";
+      this.inputPassword = "";
+      this.addRedactorModalShow = true;
+    },
+    getSubordinates: function() {
       this.loading = true;
       this.$http({
-        url: '/api/users/redactors/',
+        url: "/api/users/redactors/",
         headers: {
-          'Authorization': localStorage.getItem('jwt')
+          Authorization: localStorage.getItem("jwt")
         }
-      }).then(response => {
-        if(response.status === 200) {
-          console.log(response.data);
-          this.subordinates = response.data;
-        }
-      }).catch(function(error) {
-        if(error.response.status === 403) {
-          //TODO: Handle non authorized error
-          console.log("403 error")
-        } else if (error.response.status === 500) {
-          //TODO: Handle backend error
-          console.log("Backend error")
-        }
-      }).then(function() {
-        // this.loading = false;
       })
+        .then(response => {
+          if (response.status === 200) {
+            console.log(response.data);
+            this.subordinates = response.data;
+          }
+        })
+        .catch(function(error) {
+          if (error.response.status === 403) {
+            //TODO: Handle non authorized error
+            console.log("403 error");
+          } else if (error.response.status === 500) {
+            //TODO: Handle backend error
+            console.log("Backend error");
+          }
+        })
+        .then(function() {
+          // this.loading = false;
+        });
+    },
+    addRedactor: function() {
+      this.newRedactor.username = this.inputUsername;
+      this.newRedactor.password = this.inputPassword;
+      this.newRedactor.firstName = this.inputFirstName;
+      this.newRedactor.lastName = this.inputLastName;
+      console.log(localStorage.getItem("jwt"));
+      this.$http({
+        url: "/api/users/redactors/",
+        method: "POST",
+        headers: {
+          Authorization: localStorage.getItem("jwt"),
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        data: JSON.stringify(this.newRedactor)
+      })
+        .then(response => {
+          if (response.status === 200) {
+            console.log("New redactor added!");
+            this.getSubordinates();
+          }
+        })
+        .catch(function(error) {
+          if (error.response.status === 403) {
+            //TODO: Handle non authorized error
+            console.log("403 error");
+          } else if (error.response.status === 500) {
+            //TODO: Handle backend error
+            console.log("Backend error");
+          }
+        })
+        .then(function() {
+          // this.loading = false;
+        });
     }
   },
-  mounted:function() {
+  mounted: function() {
     this.getSubordinates();
-  },
+  }
 };
 </script>
 
@@ -60,4 +182,15 @@ ul {
   align-items: center;
 }
 
+.margin-bottom {
+  margin-bottom: 30px;
+}
+
+.button-left-margin {
+  margin-left: 10px;
+}
+
+.bottom-margin {
+  margin-bottom: 10px;
+}
 </style>
