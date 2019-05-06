5d2d9a19680a550f545484903d046ea724e29637
jerrrrycho <jurek.pakula@gmail.com>
2019-05-06 11:44:23 +0200

    Added positions in test selection

--- frontend/src/views/SelectTest.vue
+++ frontend/src/views/SelectTest.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
-    <h4>Select your language</h4>
+
     <b-form>
-      <b-dropdown id="dropdown-role" :text="onSelectedOption" class="m-md-2">
+      <h4>Select your language</h4>
+      <b-dropdown id="dropdown-role" :text="onSelectedLanguageOption" class="m-md-2">
         <b-dropdown-item
           v-for="role in availableLanguages"
+          :key="role.id"
+          :value="role.name"
+          @click="onSelectedOption = role.name"
+        >{{role.name}}</b-dropdown-item>
+      </b-dropdown>
+      <br />
+      <h4>Select your position</h4>
+      <b-dropdown id="dropdown-pos" :text="onSelectedPositionOption" class="m-md-2">
+        <b-dropdown-item
+          v-for="role in availablePositions"
           :key="role.id"
           :value="role.name"
           @click="onSelectedOption = role.name"
@@ -21,14 +32,16 @@
         name: "SelectTest",
       data() {
         return {
-          onSelectedOption: "Choose language",
-          availableLanguages: []
+          onSelectedLanguageOption: "Choose language",
+          onSelectedPositionOption: "Choose position",
+          availableLanguages: [],
+          availablePositions: []
         };
       },
       methods: {
         getLanguages: function() {
           this.$http({
-            url: 'http://localhost:8080/api/languages',
+            url: 'http://localhost:8088/api/languages',
             headers: {
               'Authorization': localStorage.getItem('jwt')
             }
@@ -49,14 +62,38 @@
             // this.loading = false;
           })
         },
+        getPositions: function() {
+          this.$http({
+            url: 'http://localhost:8088/api/positions',
+            headers: {
+              'Authorization': localStorage.getItem('jwt')
+            }
+          }).then(response => {
+            if(response.status === 200) {
+              console.log(response.data);
+              this.availablePositions = response.data;
+            }
+          }).catch(function(error) {
+            if(error.response.status === 403) {
+              //TODO: Handle non authorized error
+              console.log("403 error")
+            } else if (error.response.status === 500) {
+              //TODO: Handle backend error
+              console.log("Backend error")
+            }
+          }).then(function() {
+            // this.loading = false;
+          })
+        },
         languageSelected: function() {
 
         }
       },
-      mounted() {
-          this.getLanguages();
-      }
-    }
+    mounted () {
+      this.getLanguages(),
+      this.getPositions()
+  }
+}
 </script>
 
 <style scoped>
