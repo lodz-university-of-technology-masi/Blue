5af79d11fa1a80b9805baab2505ce1e3c8d23180
gorczii <joannagorczak@onet.pl>
2019-06-15 16:20:07 +0200

    Save screenshot - backend

--- backend/src/main/java/com/masiblue/backend/service/UsabilityDataService.java
+++ backend/src/main/java/com/masiblue/backend/service/UsabilityDataService.java
@@ -3,16 +3,24 @@ package com.masiblue.backend.service;
 import com.masiblue.backend.exception.IncorrectUsabilityDataFormat;
 import com.masiblue.backend.model.UsabilityData;
 import com.masiblue.backend.repository.UsabilityDataRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Service;
+import sun.misc.BASE64Decoder;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Service
 public class UsabilityDataService {
 
-    @Autowired
+    private static final String BASE64_HEADER = ";base64,";
+    private static final String SCREENSHOT_IMG_EXTENSION = ".jpg";
     private UsabilityDataRepository usabilityDataRepository;
+
+    public UsabilityDataService(UsabilityDataRepository usabilityDataRepository) {
+        this.usabilityDataRepository = usabilityDataRepository;
+    }
 
     public void addUsabilityData(String userName, UsabilityData usabilityData) throws IncorrectUsabilityDataFormat {
         if (usabilityData.getIsFailed() != 0 && usabilityData.getIsFailed() != 1) {
@@ -25,5 +33,23 @@ public class UsabilityDataService {
         usabilityData.setUserMeasurementNumber(userMeasurementNumber);
 
         usabilityDataRepository.save(usabilityData);
+    }
+
+    public String saveScreenshot(String userName, String decodedImg) throws IOException {
+        if (decodedImg.contains(BASE64_HEADER)) {
+            int index = decodedImg.lastIndexOf(BASE64_HEADER) + BASE64_HEADER.length();
+            decodedImg = decodedImg.substring(index);
+        }
+
+        long timestamp = System.currentTimeMillis() / 1000;
+        String fileName = userName + "_" + timestamp;
+        String filePath = "./../" + fileName + SCREENSHOT_IMG_EXTENSION;
+        File outputFile = new File(filePath);
+        byte[] imageByte;
+        BASE64Decoder decoder = new BASE64Decoder();
+        imageByte = decoder.decodeBuffer(decodedImg);
+        FileUtils.writeByteArrayToFile(outputFile, imageByte);
+
+        return fileName;
     }
 }
