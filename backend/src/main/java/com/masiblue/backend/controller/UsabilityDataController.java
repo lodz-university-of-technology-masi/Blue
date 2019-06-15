16fc0f6e03e2ef0b586b2ac0e168122e6f5344a6
gorczii <joannagorczak@onet.pl>
2019-06-15 14:27:54 +0200

    Usability data - backend

---
+++ backend/src/main/java/com/masiblue/backend/controller/UsabilityDataController.java
@@ -0,0 +1,34 @@
+package com.masiblue.backend.controller;
+
+import com.masiblue.backend.exception.IncorrectUsabilityDataFormat;
+import com.masiblue.backend.model.UsabilityData;
+import com.masiblue.backend.service.UsabilityDataService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/metrics")
+public class UsabilityDataController {
+
+    private UsabilityDataService usabilityDataService;
+
+    public UsabilityDataController(UsabilityDataService usabilityDataService) {
+        this.usabilityDataService = usabilityDataService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addNewPosition(Authentication auth, @RequestBody UsabilityData usabilityData) {
+        try {
+            usabilityDataService.addUsabilityData(auth.getName(), usabilityData);
+        } catch (IncorrectUsabilityDataFormat ex) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage(), ex);
+        }
+        return new ResponseEntity<>("Successfully added new usability data", HttpStatus.OK);
+    }
+}
